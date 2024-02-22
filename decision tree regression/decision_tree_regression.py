9ef3dfa96a372c6535116fb742089666b4f1a6ec
Mohsin Tariq <mohsin@luminogics.com>
2024-02-22 12:27:48 +0500

    decision tree regression model

---
+++ decision tree regression/decision_tree_regression.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+dataset = pd.read_csv('Position_Salaries.csv')
+X=dataset.iloc[:,1:-1].values
+y=dataset.iloc[:,-1].values
+
+print(X)
+print(y)
+
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X,y)
+
+print(regressor.predict([[6.5]]))
+
+# Visualising the Decision Tree Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
